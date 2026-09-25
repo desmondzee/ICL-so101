d0fe651ea062b2d84b08fd68f4c4bd40e26c860f
desmondzee <desmond.zee@gmail.com>
2026-09-25 01:14:52 +0100

    Tilt overhead camera to 60 degrees and regenerate examples

--- sim/libero_basket_env.py
+++ sim/libero_basket_env.py
@@ -32,7 +32,7 @@ TASK = "put both the alphabet soup and the cream cheese box in the basket"
 WRIST_CAM_POS = (0.0025, 0.06157, -0.01877)
 WRIST_CAM_QUAT = (0.97616, -0.21706, 0.0, 0.0)
 WRIST_CAM_FOVY = 48.46
-OVERHEAD_ELEVATION = -75.0
+OVERHEAD_ELEVATION = -60.0
 
 
 def default_config(width=640, height=480):
